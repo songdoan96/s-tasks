cca83f9230003374f0c11f80b0161998ad47b1ff
songdoan96 <songdoan96@gmail.com>
2022-06-11 15:27:39 +0700

    utcToLocal

--- helpers/hbsHelpers.js
+++ helpers/hbsHelpers.js
@@ -12,7 +12,7 @@ const getFlag = (flag) => {
   }
 };
 function utcToLocal(time, format) {
-  return moment(time).tz("Asia", "Ho_Chi_Minh").format(format);
+  return moment(time).locale("vi").local(true).format(format);
 }
 
 module.exports = { getFlag, utcToLocal };
